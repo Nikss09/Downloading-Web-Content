9b56862e067749fbbc7f95b8a03b85b0d497a819
Nikss09 <nikhilakhurana@gmail.com>
2021-01-28 16:52:37 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.downloadingwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    //in AsyncTask both 'String... String' means input is string and output is string respectively
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result="";
+            URL url;
+            HttpURLConnection urlConnection=null;
+
+            try {
+                url=new URL(strings[0]);
+                urlConnection=(HttpURLConnection)url.openConnection();
+                InputStream in=urlConnection.getInputStream();
+                InputStreamReader reader=new InputStreamReader(in);
+                int data=reader.read();
+
+                while(data!=-1){
+                    char current=(char)data;
+                    result+=current;
+                    data=reader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task=new DownloadTask();
+        String result=null;
+        String website;
+        TextView textView=findViewById(R.id.textview);
+
+        try {
+            website="https://zappycode.com/";
+            result=task.execute(website).get();
+            Log.i("Result",""+result);
+            textView.setText(website+"\n............\n"+result);
+        }
+        catch(Exception e){
+            Log.i("Error","Exception"+e);
+        }
+    }
+}
